b8d4167d94cb51933b17a54e409b62e622ae4ae5
Luan alves <luan_alves@hotmail.com>
2025-11-21 10:47:19 -0300

    refactor: remove old api_gateway module directory
    - Delete all files from api_gateway/ directory
    - Module renamed to thedevkitchen_apigateway
    - All functionality migrated to new module
    - Database migration completed successfully

--- 18.0/extra-addons/api_gateway/controllers/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import auth_controller
-from . import test_controller
-from . import swagger_controller
--- 18.0/extra-addons/api_gateway/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import oauth_application
-from . import oauth_token
-from . import api_endpoint
-from . import api_access_log
