ff27de273baf50e669d239ad2e795bf415003860
Luan alves <luan_alves@hotmail.com>
2026-05-26 09:55:13 -0300

    remove tests/api directory and clean up imports from __init__.py

--- 18.0/extra-addons/quicksol_estate/tests/__init__.py
+++ 18.0/extra-addons/quicksol_estate/tests/__init__.py
@@ -7,14 +7,6 @@ from . import base_validation_test
 from . import base_company_test
 from . import base_agent_test
 
-# HTTP/API integration tests (tagged post_install)
-from .api import test_property_api
-from .api import test_property_api_auth
-from .api import test_master_data_api
-from .api import test_company_isolation_api
-from .api import test_property_attachments_api  # Feature 017
-from .api import test_capabilities_api  # Feature 020
-
 # Integration tests directory (TransactionCase - WITH database)
 from . import integration
 # Feature 013: Property Proposals (explicit imports for Odoo test discovery)
